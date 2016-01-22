172ffba0c1b47743f0729f9e74a17f20e6a844c8
Your Name <sbaird@sch.org>
2016-01-22 08:52:18 -0500

    instagram with errors

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchInstagramByHashtag("dogs")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    func searchInstagramByHashtag(searchString:String) {
+        let manager = AFHTTPSessionManager()
+        manager.GET("https://api.instagram.com/v1/tags/\(searchString)/media/recent?client_id=YOUR_CLIENT_ID",
+            parameters: nil,
+            progress: nil,
+            success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
+                if let responseObject = responseObject {
+                    if let dataArray = responseObject["data"] as? [AnyObject] {
+                        var urlArray:[String] = []
+                        for dataObject in dataArray {
+                            if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                                urlArray.append(imageURLString)
+                            }
+                        }
+                        
+                        self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                        
+                        for var i = 0; i < urlArray.count; i++ {
+                            let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))
+                            if let url = NSURL(string: urlArray[i]) {
+                                imageView.setImageWithURL( url)
+                                self.scrollView.addSubview(imageView)
+                            }
+                        }
+                        
+                    }
+                }
+            },
+            failure: { (operation: NSURLSessionDataTask?,error: NSError) in
+                print("Error: " + error.localizedDescription)
+        })
+        
     }
-
-
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        if let searchText = searchBar.text {
+            searchInstagramByHashtag(searchText)
+        }
+        
+    }
 }
-
---
+++ bridge.h
@@ -0,0 +1,16 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Samira Baird on 1/15/16.
+//  Copyright © 2016 Samira Baird. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+
+
+
+
+#endif /* bridge_h */
